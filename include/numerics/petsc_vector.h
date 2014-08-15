2d8a909ad4ecf3e92ce4eb2a01c798f40ed5f251
John Peterson <jwpeterson@gmail.com>
2014-08-15 14:11:40 -0600

    Adding AutoPtr as preprocessor macro, fixing invalid AutoPtr use in library and examples.
    C++11 supports alias declarations which would make this a bit more elegant, viz
    
      #ifdef LIBMESH_HAVE_CXX11_UNIQUE_PTR
        template <typename T>
        using AutoPtr = std::unique_ptr<T>;
      #elif LIBMESH_HAVE_HINNANT_UNIQUE_PTR
        template <typename T>
        using AutoPtr = std::boost_ptr<T>;
      #endif
    
    but we can't require this capability in the library yet.
    
    The following invalid uses of unique_ptr-based AutoPtr are fixed in library and examples:
    
    1.) The copy constructor is explicitly disallowed by this new AutoPtr.
        The kludge once required for ancient xlC compilers:
    
        AutoPtr<X> ap(new X);
        return ap;
    
        is replaced by
    
        return AutoPtr<X>(new X);
    
        Note that we have not been consistent in using this kludge for a while
        now (see various Elem::build_edge() implementations) and no one has
        complained...
    
    2.) It is ambiguous to construct a std::unique_ptr with an explicit (NULL) argument.
        This is apparently because there is a 'nullptr' constructor for this
        class.  Luckily, using the default constructor instead works just fine.

--- include/numerics/petsc_vector.h
+++ include/numerics/petsc_vector.h
@@ -1041,12 +1041,9 @@ template <typename T>
 inline
 AutoPtr<NumericVector<T> > PetscVector<T>::zero_clone () const
 {
-  AutoPtr<NumericVector<T> > cloned_vector
-    (new PetscVector<T>(this->comm(), this->type()));
-
+  NumericVector<T>* cloned_vector = new PetscVector<T>(this->comm(), this->type());
   cloned_vector->init(*this);
-
-  return cloned_vector;
+  return AutoPtr<NumericVector<T> >(cloned_vector);
 }
 
 
@@ -1055,14 +1052,10 @@ template <typename T>
 inline
 AutoPtr<NumericVector<T> > PetscVector<T>::clone () const
 {
-  AutoPtr<NumericVector<T> > cloned_vector
-    (new PetscVector<T>(this->comm(), this->type()));
-
+  NumericVector<T>* cloned_vector = new PetscVector<T>(this->comm(), this->type());
   cloned_vector->init(*this, true);
-
   *cloned_vector = *this;
-
-  return cloned_vector;
+  return AutoPtr<NumericVector<T> >(cloned_vector);
 }
 
 
